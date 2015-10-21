ba3bcaf7d39eff4bc03065b5401665fbe1c74158
Ashutosh Das <git.pyprism@gmail.com>
2015-10-21 15:33:11 +0600

    removed all codes

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var cons = require('consolidate');
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Welcome to Hiren Picture !' });
-});
-
-module.exports = router;
