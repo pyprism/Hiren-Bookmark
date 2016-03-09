5e7684853ce6eb1995f2289d9be89b7c0b5b9b06
Ashutosh Das <git.pyprism@gmail.com>
2016-03-09 23:18:00 +0600

    Added new conf

--- hiren.js
+++ hiren.js
@@ -1,7 +1,8 @@
 /**
  * Created by prism on 3/7/16.
  */
-var express = require('express');
+var express = require('express'),
+    Sequelize = require('sequelize');
 
 try {
     var config = require('./config.local.json');
@@ -9,11 +10,15 @@ try {
     var config = require('./config.json');
 }
 
+var sequelize = new Sequelize('postgres://' + config.db_user + ':' + config.db_password + '@localhost:5432/' + config.db_name);
+var Account = require('./models/Account')(sequelize);
+var auth = require('./routes/auth');
 var port = process.env.PORT || 3000;
 
 express()
     .enable('trust proxy')
     .set('view engine', 'ejs')
+    .use(require('helmet')())
     .use(express.static('./public'))
     .use(require('body-parser').json())
     .use(require('cookie-parser')())
