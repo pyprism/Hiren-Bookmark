f238fd8259f5d2d599a9db178c4ca6ad07443136
Ashutosh Das <git.pyprism@gmail.com>
2016-03-15 17:55:10 +0600

    changed orm

--- models/Account.js
+++ models/Account.js
@@ -1,16 +1,12 @@
 /**
  * Created by prism on 3/9/16.
  */
-var Sequelize = require('sequelize');
 
-module.exports = function(sequelize) {
-       var Account = sequelize.define('account', {
-            username: Sequelize.STRING,
-            password: Sequelize.STRING
-        },{
-            freezeTableName: true
-        });
-    sequelize.sync();
+module.exports = function(bookshelf) {
+       var Account = bookshelf.Model.extend({
+           tableName: 'Account',
+           hasTimestamps: true
+       });
 
     return Account;
 };
