2c5fde019cfaad9032c3c888ed4969fbdbe694db
Ashutosh Das <git.pyprism@gmail.com>
2016-03-20 23:39:15 +0600

    fixed http status code

--- routes/tags.js
+++ routes/tags.js
@@ -3,14 +3,14 @@
  */
 var express = require('express');
 
-module.exports = function(Tags, Urls) {
+module.exports = function(Tags) {
     var router = express.Router();
 
     //route for get all tags
     router.route('/')
     .get(function(req, res) {
         Tags.find().then(function(tags) {
-            res.status(201).send({ tags: tags});
+            res.status(200).send({ tags: tags});
         });
 
     })
@@ -34,11 +34,9 @@ module.exports = function(Tags, Urls) {
     .put(function(req, res) { //update single tag
        Tags.findById(req.params.id).then(function(result) {
            if(!result) return res.status(404).send({error: 'tag not found'});
-           console.log(result);
-           console.log(req.body.name);
            result.name = req.body.name;
            result.save();
-           res.status(204).send({message: 'tag updated'});
+           res.status(202).send({message: 'tag updated'});
        })
     })
     .delete(function(req, res) {  //delete single tag
