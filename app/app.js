f52c1995e2787b395cff822ae69466f1556f8b31
Ashutosh Das <git.pyprism@gmail.com>
2016-03-30 21:42:19 +0600

    added new route

--- app/app.js
+++ app/app.js
@@ -5,6 +5,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Login from './componets/Login.jsx';
 import Dashboard from './componets/Dashboard.jsx';
+import Url from './componets/Url.jsx';
 import {Router, Route, browserHistory} from 'react-router';
 
 
@@ -13,6 +14,7 @@ ReactDOM.render(
     <Router history={browserHistory} >
         <Route path="/" component={Login}> </Route>
         <Route path="/dashboard" component={Dashboard}> </Route>
+        <Route path="/dashboard/tags/:tag_id" component={Url}> </Route>
     </Router>,
     document.getElementById('app')
 );
