775b7924acc3db82fda4f8cd309c54c369b34333
Ashutosh Das <git.pyprism@gmail.com>
2016-03-31 23:02:23 +0600

    ****

--- app/componets/Url.jsx
+++ app/componets/Url.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Helmet from "react-helmet";
 import {Link} from 'react-router';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import Sidebar from './Sidebar.jsx';
 import Urls from '../ajax/Urls.jsx';
 
@@ -8,31 +9,21 @@ export default class extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            urls: {},
+            urls: [],
             loaded: false
         };
     }
 
     componentDidMount() {
         Urls.getUrls(this.props.params.tag_id).done(urls => {
-            console.log(urls);
+
             this.setState({urls: urls || [], loaded: true});
-        })
+        });
+
 
     }
 
     render() {
-
-        let items = this.state.loaded? (this.state.urls).map(url =>
-            <tbody>
-                <tr key={url._id}>
-                <td>{url.name}</td>
-                <td>{url.url}</td>
-                <td>{url.date}</td>
-                </tr>
-            </tbody>
-        ): [<li key="loading"> <em>Loading......</em> </li>];
-
         return (
             <div id="wrapper">
                 <Helmet
@@ -40,38 +31,20 @@ export default class extends React.Component {
                     link={[
                     {"rel": "stylesheet", "href": "/css/bootstrap.min.css"},
                     {"rel": "stylesheet", "href": "/css/simple-sidebar.css"},
-                    {"rel": "stylesheet", "href": "/css/font-awesome.min.css"}
+                    {"rel": "stylesheet", "href": "/css/font-awesome.min.css"},
+                    {"rel": "stylesheet", "href": "/css/react-bootstrap-table.min.css"}
                 ]}
                 />
                 <Sidebar/>
-                
-                
-                {
-                    <div class='container'>
-                    <thead>
-                <tr>
-                <th>Title</th>
-                <th>Url</th>
-                <th>Date</th>
-                </tr>
-                </thead>
-                <tbody>
-                    this.state.loaded? (this.state.urls).map(url =>
-                    <table className="table table-striped">
-                
-                <tr>
-                <td>{url.name}</td>
-                <td>{url.url}</td>
-                <td>{url.date}</td>
-                </tr>
-                </tbody>
-             </table>
+                    
+                    
+                     <BootstrapTable data={this.state.urls} striped={true} hover={true} bordered={true}>
+                     <TableHeaderColumn dataField="title" isKey={true} dataAlign="center" dataSort={true}>{this.state.urls['title']} Title</TableHeaderColumn>
+                       <TableHeaderColumn dataField="url" dataSort={true}> <a href={this.state.urls['url']}>{this.state.urls['url']}</a> Link</TableHeaderColumn>
+                      <TableHeaderColumn dataField="date" >{this.state.urls['date']} Date Added</TableHeaderColumn>
+                     </BootstrapTable>
 
-        ) : [<li key="loading"> <em>Loading......</em> </li>]
-              </div>
-                }
-               
-               
+                    
                 
             </div>
         )
