49f80c943252fc52c4e5215f26d5c9286fa16f18
Ashutosh Das <git.pyprism@gmail.com>
2016-04-02 22:25:13 +0600

    cleanup

--- app/ajax/Tags.jsx
+++ app/ajax/Tags.jsx
@@ -16,4 +16,25 @@ export default class Tags {
 
         });
     }
+
+    static createTag(name) {
+        return $.ajax({
+            method: 'POST',
+            url: '/tags/',
+            data: {tag: name},
+            'beforeSend': function(xhr) {
+                if (sessionStorage.getItem('token')) {
+                    xhr.setRequestHeader('x-access-token', sessionStorage.getItem('token'));
+                }}
+        }).done(function(response){
+            console.log(response);
+            location.reload(true); //TODO handle page refresh via state
+        }).fail(function(err) {
+            console.log(err);
+            alert('Duplicate tag');
+            //window.location.href = "/";
+            //alert(err.responseJSON.error);
+
+        });
+    }
 }
