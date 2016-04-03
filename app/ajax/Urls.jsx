f24b79279a48bd0cfbc2636f040cdfbd051147e4
Ashutosh Das <git.pyprism@gmail.com>
2016-04-03 20:05:23 +0600

    added new method

--- app/ajax/Urls.jsx
+++ app/ajax/Urls.jsx
@@ -16,4 +16,19 @@ export default class Urls {
         });
 
     }
+
+    static createUrl(tag_id, url) {
+        return $.ajax({
+            method: 'POST',
+            url: '/urls/',
+            cache: false,
+            data:{ tag_id: tag_id, url:url},
+            'beforeSend': function(xhr) {
+                if (sessionStorage.getItem('token')) {
+                    xhr.setRequestHeader('x-access-token', sessionStorage.getItem('token'));
+                }}
+        }).fail(function(err) {
+            alert(err);
+        });
+    }
 }
